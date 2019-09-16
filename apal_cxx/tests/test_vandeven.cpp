6e6e5b21c843756146726bcd8ccfc34471ad575e
David Kleiven <davidkleiven446@gmail.com>
2019-09-16 11:38:01 +0200

    Fix according to max value

--- apal_cxx/tests/test_vandeven.cpp
+++ apal_cxx/tests/test_vandeven.cpp
@@ -8,7 +8,7 @@ PyObject* test_order1(){
     Vandeven vandeven(1);
     double dx = 1.0/9.0;
     for (unsigned int i=0;i<10;i++){
-        double value = vandeven.evaluate(i*dx*PI/2);
+        double value = vandeven.evaluate(i*dx*PI);
         double expect = 1.0 - i*dx;
         if (abs(value - expect) > 1E-6){
             Py_RETURN_FALSE;
@@ -23,7 +23,7 @@ PyObject* test_order2(){
     double dx = 1.0/9.0;
     for (unsigned int i=0;i<10;i++){
         double x = i*dx;
-        double value = vandeven.evaluate(x*PI/2);
+        double value = vandeven.evaluate(x*PI);
         double expect = 1.0  - 6*(x*x/2 - x*x*x/3);
 
         if (abs(value - expect) > 1E-4){
